063dd5f2ec4c3d517f352ee9b8237f4ee4acbc22
Tahmid Efaz <tefaz@redhat.com>
2021-09-29 16:59:24 -0400

    moving common functions to utils

--- internal/endpoints/payloads.go
+++ internal/endpoints/payloads.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"github.com/go-chi/chi/v5"
 	"net/http"
-	"strconv"
 	"strings"
 	"time"
 
@@ -25,87 +24,6 @@ var (
 	RetrievePayloads          = db_methods.RetrievePayloads
 	RetrieveRequestIdPayloads = db_methods.RetrieveRequestIdPayloads
 )
-
-// initQuery intializes the query with default values
-func initQuery(r *http.Request) (structs.Query, error) {
-
-	q := structs.Query{
-		Page:         0,
-		PageSize:     10,
-		SortBy:       "created_at",
-		SortDir:      "desc",
-		InventoryID:  r.URL.Query().Get("inventory_id"),
-		SystemID:     r.URL.Query().Get("system_id"),
-		CreatedAtLT:  r.URL.Query().Get("created_at_lt"),
-		CreatedAtGT:  r.URL.Query().Get("created_at_gt"),
-		CreatedAtLTE: r.URL.Query().Get("created_at_lte"),
-		CreatedAtGTE: r.URL.Query().Get("created_at_gte"),
-		Account:      r.URL.Query().Get("account"),
-	}
-
-	var err error
-
-	if r.URL.Query().Get("sort_by") != "" || stringInSlice(r.URL.Query().Get("sort_by"), validSortBy) {
-		q.SortBy = r.URL.Query().Get("sort_by")
-	}
-
-	if r.URL.Query().Get("sort_dir") != "" || stringInSlice(r.URL.Query().Get("sort_dir"), validSortDir) {
-		q.SortDir = r.URL.Query().Get("sort_dir")
-	}
-
-	if r.URL.Query().Get("page") != "" {
-		q.Page, err = strconv.Atoi(r.URL.Query().Get("page"))
-	}
-
-	if r.URL.Query().Get("page_size") != "" {
-		q.PageSize, err = strconv.Atoi(r.URL.Query().Get("page_size"))
-	}
-
-	return q, err
-}
-
-func getErrorBody(message string, status int) string {
-	errBody := structs.ErrorResponse{
-		Title:   http.StatusText(status),
-		Message: message,
-		Status:  status,
-	}
-
-	errBodyJson, _ := json.Marshal(errBody)
-	return string(errBodyJson)
-}
-
-// Check for value in a slice
-func stringInSlice(a string, list []string) bool {
-	for _, b := range list {
-		if b == a {
-			return true
-		}
-	}
-	return false
-}
-
-// Check timestamp format
-func validTimestamps(q structs.Query) bool {
-	timestampQueries := []string{q.CreatedAtLT, q.CreatedAtGT, q.CreatedAtLTE, q.CreatedAtGTE}
-
-	for _, ts := range timestampQueries {
-		if ts != "" {
-			_, err := time.Parse(time.RFC3339, ts)
-			if err != nil {
-				return false
-			}
-		}
-	}
-	return true
-}
-
-// Write HTTP Response
-func writeResponse(w http.ResponseWriter, status int, message string) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(status)
-	w.Write([]byte(message))
-}
 
 // Payloads returns responses for the /payloads endpoint
 func Payloads(w http.ResponseWriter, r *http.Request) {
