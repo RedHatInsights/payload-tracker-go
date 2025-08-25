d418ea61dae678eec37cbca13b0cddfe279212a9
Derek Horton <dehort@redhat.com>
2025-08-25 14:23:25 -0500

    Tweaks after testing

--- migrations/000001_create_payload_tracker_tables.up.sql
+++ migrations/000001_create_payload_tracker_tables.up.sql
@@ -1,4 +1,4 @@
-CREATE FUNCTION create_partition(start timestamp with time zone, stop timestamp with time zone) RETURNS void
+CREATE FUNCTION public.create_partition(start timestamp with time zone, stop timestamp with time zone) RETURNS void
     LANGUAGE plpgsql
     AS $$
             DECLARE
@@ -18,7 +18,7 @@ CREATE FUNCTION create_partition(start timestamp with time zone, stop timestamp 
         $$;
 
 
-CREATE FUNCTION drop_partition(start timestamp with time zone, stop timestamp with time zone) RETURNS void
+CREATE FUNCTION public.drop_partition(start timestamp with time zone, stop timestamp with time zone) RETURNS void
     LANGUAGE plpgsql
     AS $$
             DECLARE
@@ -31,7 +31,7 @@ CREATE FUNCTION drop_partition(start timestamp with time zone, stop timestamp wi
         $$;
 
 
-CREATE FUNCTION get_date_string(value timestamp with time zone) RETURNS text
+CREATE FUNCTION public.get_date_string(value timestamp with time zone) RETURNS text
     LANGUAGE plpgsql
     AS $$
             BEGIN
@@ -46,7 +46,7 @@ CREATE FUNCTION get_date_string(value timestamp with time zone) RETURNS text
 
 
 
-CREATE TABLE payload_statuses (
+CREATE TABLE public.payload_statuses (
     id bigint NOT NULL,
     payload_id bigint NOT NULL,
     service_id integer NOT NULL,
@@ -59,7 +59,7 @@ CREATE TABLE payload_statuses (
 PARTITION BY RANGE (date);
 
 
-CREATE SEQUENCE payload_statuses_id_seq
+CREATE SEQUENCE public.payload_statuses_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -67,10 +67,10 @@ CREATE SEQUENCE payload_statuses_id_seq
     CACHE 1;
 
 
-ALTER SEQUENCE payload_statuses_id_seq OWNED BY payload_statuses.id;
+ALTER SEQUENCE public.payload_statuses_id_seq OWNED BY public.payload_statuses.id;
 
 
-CREATE TABLE payloads (
+CREATE TABLE public.payloads (
     id bigint NOT NULL,
     request_id character varying NOT NULL,
     account character varying,
@@ -81,7 +81,7 @@ CREATE TABLE payloads (
 );
 
 
-CREATE SEQUENCE payloads_id_seq
+CREATE SEQUENCE public.payloads_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -89,16 +89,16 @@ CREATE SEQUENCE payloads_id_seq
     CACHE 1;
 
 
-ALTER SEQUENCE payloads_id_seq OWNED BY payloads.id;
+ALTER SEQUENCE public.payloads_id_seq OWNED BY public.payloads.id;
 
 
-CREATE TABLE services (
+CREATE TABLE public.services (
     id integer NOT NULL,
     name character varying NOT NULL
 );
 
 
-CREATE SEQUENCE services_id_seq
+CREATE SEQUENCE public.services_id_seq
     AS integer
     START WITH 1
     INCREMENT BY 1
@@ -107,15 +107,15 @@ CREATE SEQUENCE services_id_seq
     CACHE 1;
 
 
-ALTER SEQUENCE services_id_seq OWNED BY services.id;
+ALTER SEQUENCE public.services_id_seq OWNED BY public.services.id;
 
 
-CREATE TABLE sources (
+CREATE TABLE public.sources (
     id integer NOT NULL,
     name character varying NOT NULL
 );
 
-CREATE SEQUENCE sources_id_seq
+CREATE SEQUENCE public.sources_id_seq
     AS integer
     START WITH 1
     INCREMENT BY 1
@@ -124,16 +124,16 @@ CREATE SEQUENCE sources_id_seq
     CACHE 1;
 
 
-ALTER SEQUENCE sources_id_seq OWNED BY sources.id;
+ALTER SEQUENCE public.sources_id_seq OWNED BY public.sources.id;
 
 
-CREATE TABLE statuses (
+CREATE TABLE public.statuses (
     id integer NOT NULL,
     name character varying NOT NULL
 );
 
 
-CREATE SEQUENCE statuses_id_seq
+CREATE SEQUENCE public.statuses_id_seq
     AS integer
     START WITH 1
     INCREMENT BY 1
@@ -142,128 +142,118 @@ CREATE SEQUENCE statuses_id_seq
     CACHE 1;
 
 
-ALTER SEQUENCE statuses_id_seq OWNED BY statuses.id;
+ALTER SEQUENCE public.statuses_id_seq OWNED BY public.statuses.id;
 
 
-ALTER TABLE ONLY payload_statuses ALTER COLUMN id SET DEFAULT nextval('payload_statuses_id_seq'::regclass);
+ALTER TABLE ONLY public.payload_statuses ALTER COLUMN id SET DEFAULT nextval('public.payload_statuses_id_seq'::regclass);
 
-ALTER TABLE ONLY payloads ALTER COLUMN id SET DEFAULT nextval('payloads_id_seq'::regclass);
+ALTER TABLE ONLY public.payloads ALTER COLUMN id SET DEFAULT nextval('public.payloads_id_seq'::regclass);
 
-ALTER TABLE ONLY services ALTER COLUMN id SET DEFAULT nextval('services_id_seq'::regclass);
+ALTER TABLE ONLY public.services ALTER COLUMN id SET DEFAULT nextval('public.services_id_seq'::regclass);
 
-ALTER TABLE ONLY sources ALTER COLUMN id SET DEFAULT nextval('sources_id_seq'::regclass);
+ALTER TABLE ONLY public.sources ALTER COLUMN id SET DEFAULT nextval('public.sources_id_seq'::regclass);
 
-ALTER TABLE ONLY statuses ALTER COLUMN id SET DEFAULT nextval('statuses_id_seq'::regclass);
-
-
-SELECT pg_catalog.setval('payload_statuses_id_seq', 1, false);
-
-SELECT pg_catalog.setval('payloads_id_seq', 1, false);
-
-SELECT pg_catalog.setval('services_id_seq', 13, true);
-
-SELECT pg_catalog.setval('sources_id_seq', 4, true);
-
-SELECT pg_catalog.setval('statuses_id_seq', 8, true);
+ALTER TABLE ONLY public.statuses ALTER COLUMN id SET DEFAULT nextval('public.statuses_id_seq'::regclass);
 
 
-ALTER TABLE ONLY services
+SELECT pg_catalog.setval('public.payload_statuses_id_seq', 1, false);
+
+SELECT pg_catalog.setval('public.payloads_id_seq', 1, false);
+
+SELECT pg_catalog.setval('public.services_id_seq', 13, true);
+
+SELECT pg_catalog.setval('public.sources_id_seq', 4, true);
+
+SELECT pg_catalog.setval('public.statuses_id_seq', 8, true);
+
+
+ALTER TABLE ONLY public.services
     ADD CONSTRAINT idx_services_name UNIQUE (name);
 
-ALTER TABLE ONLY sources
+ALTER TABLE ONLY public.sources
     ADD CONSTRAINT idx_sources_name UNIQUE (name);
 
-ALTER TABLE ONLY statuses
+ALTER TABLE ONLY public.statuses
     ADD CONSTRAINT idx_statuses_name UNIQUE (name);
 
-ALTER TABLE ONLY payload_statuses
+ALTER TABLE ONLY public.payload_statuses
     ADD CONSTRAINT payload_statuses_pkey PRIMARY KEY (id, date);
 
-ALTER TABLE ONLY payloads
+ALTER TABLE ONLY public.payloads
     ADD CONSTRAINT payloads_pkey PRIMARY KEY (id);
 
-ALTER TABLE ONLY payloads
+ALTER TABLE ONLY public.payloads
     ADD CONSTRAINT payloads_request_id_key UNIQUE (request_id);
 
-ALTER TABLE ONLY services
+ALTER TABLE ONLY public.services
     ADD CONSTRAINT services_name_key UNIQUE (name);
 
-ALTER TABLE ONLY services
+ALTER TABLE ONLY public.services
     ADD CONSTRAINT services_pkey PRIMARY KEY (id);
 
-ALTER TABLE ONLY sources
+ALTER TABLE ONLY public.sources
     ADD CONSTRAINT sources_name_key UNIQUE (name);
 
-ALTER TABLE ONLY sources
+ALTER TABLE ONLY public.sources
     ADD CONSTRAINT sources_pkey PRIMARY KEY (id);
 
-ALTER TABLE ONLY statuses
+ALTER TABLE ONLY public.statuses
     ADD CONSTRAINT statuses_name_key UNIQUE (name);
 
-ALTER TABLE ONLY statuses
+ALTER TABLE ONLY public.statuses
     ADD CONSTRAINT statuses_pkey PRIMARY KEY (id);
 
 
-CREATE INDEX payload_statuses_service_id_created_at_idx ON ONLY payload_statuses USING btree (service_id, created_at);
 
-CREATE INDEX payload_statuses_service_id_date_idx ON ONLY payload_statuses USING btree (service_id, date);
+CREATE INDEX payload_statuses_service_id_created_at_idx ON ONLY public.payload_statuses USING btree (service_id, created_at);
 
-CREATE INDEX payload_statuses_service_id_status_id_idx ON ONLY payload_statuses USING btree (service_id, status_id);
+CREATE INDEX payload_statuses_service_id_date_idx ON ONLY public.payload_statuses USING btree (service_id, date);
 
-CREATE INDEX payload_statuses_status_id_created_at_idx ON ONLY payload_statuses USING btree (status_id, created_at);
+CREATE INDEX payload_statuses_service_id_status_id_idx ON ONLY public.payload_statuses USING btree (service_id, status_id);
 
-CREATE INDEX payload_statuses_status_id_date_idx ON ONLY payload_statuses USING btree (status_id, date);
+CREATE INDEX payload_statuses_status_id_created_at_idx ON ONLY public.payload_statuses USING btree (status_id, created_at);
 
-CREATE INDEX payload_statuses_source_id_date_idx ON ONLY payload_statuses USING btree (status_id, date);
+CREATE INDEX payload_statuses_source_id_created_at_idx ON ONLY public.payload_statuses USING btree (status_id, created_at);
 
-CREATE INDEX payloads_account_idx ON payloads USING btree (account);
+CREATE INDEX payload_statuses_status_id_date_idx ON ONLY public.payload_statuses USING btree (status_id, date);
+
+CREATE INDEX payload_statuses_source_id_date_idx ON ONLY public.payload_statuses USING btree (status_id, date);
+
+CREATE INDEX payloads_account_idx ON public.payloads USING btree (account);
 
 CREATE INDEX payloads_created_at_idx ON payloads USING btree (created_at);
 
-CREATE UNIQUE INDEX payloads_id_idx ON payloads USING btree (id);
+CREATE UNIQUE INDEX payloads_id_idx ON public.payloads USING btree (id);
 
-CREATE INDEX payloads_inventory_id_idx ON payloads USING btree (inventory_id);
+CREATE INDEX payloads_inventory_id_idx ON public.payloads USING btree (inventory_id);
 
-CREATE UNIQUE INDEX payloads_request_id_idx ON payloads USING btree (request_id);
+CREATE UNIQUE INDEX payloads_request_id_idx ON public.payloads USING btree (request_id);
 
-CREATE INDEX payloads_system_id_idx ON payloads USING btree (system_id);
+CREATE INDEX payloads_system_id_idx ON public.payloads USING btree (system_id);
 
-CREATE UNIQUE INDEX services_id_idx ON services USING btree (id);
+CREATE UNIQUE INDEX services_id_idx ON public.services USING btree (id);
 
-CREATE UNIQUE INDEX services_name_idx ON services USING btree (name);
+CREATE UNIQUE INDEX services_name_idx ON public.services USING btree (name);
 
-CREATE UNIQUE INDEX sources_id_idx ON sources USING btree (id);
+CREATE UNIQUE INDEX sources_id_idx ON public.sources USING btree (id);
 
-CREATE UNIQUE INDEX sources_name_idx ON sources USING btree (name);
+CREATE UNIQUE INDEX sources_name_idx ON public.sources USING btree (name);
 
-CREATE UNIQUE INDEX statuses_id_idx ON statuses USING btree (id);
+CREATE UNIQUE INDEX statuses_id_idx ON public.statuses USING btree (id);
 
-CREATE UNIQUE INDEX statuses_name_idx ON statuses USING btree (name);
+CREATE UNIQUE INDEX statuses_name_idx ON public.statuses USING btree (name);
 
+ALTER TABLE public.payload_statuses
+    ADD CONSTRAINT payload_statuses_payload_id_fkey FOREIGN KEY (payload_id) REFERENCES public.payloads(id) ON DELETE CASCADE;
 
-ALTER TABLE payload_statuses
-    ADD CONSTRAINT fk_payload_statuses_payload FOREIGN KEY (payload_id) REFERENCES payloads(id);
+ALTER TABLE public.payload_statuses
+    ADD CONSTRAINT payload_statuses_service_id_fkey FOREIGN KEY (service_id) REFERENCES public.services(id);
 
-ALTER TABLE payload_statuses
-    ADD CONSTRAINT fk_payload_statuses_service FOREIGN KEY (service_id) REFERENCES services(id);
+ALTER TABLE public.payload_statuses
+    ADD CONSTRAINT payload_statuses_source_id_fkey FOREIGN KEY (source_id) REFERENCES public.sources(id);
 
-ALTER TABLE payload_statuses
-    ADD CONSTRAINT fk_payload_statuses_source FOREIGN KEY (source_id) REFERENCES sources(id);
-
-ALTER TABLE payload_statuses
-    ADD CONSTRAINT fk_payload_statuses_status FOREIGN KEY (status_id) REFERENCES statuses(id);
-
-ALTER TABLE payload_statuses
-    ADD CONSTRAINT payload_statuses_payload_id_fkey FOREIGN KEY (payload_id) REFERENCES payloads(id) ON DELETE CASCADE;
-
-ALTER TABLE payload_statuses
-    ADD CONSTRAINT payload_statuses_service_id_fkey FOREIGN KEY (service_id) REFERENCES services(id);
-
-ALTER TABLE payload_statuses
-    ADD CONSTRAINT payload_statuses_source_id_fkey FOREIGN KEY (source_id) REFERENCES sources(id);
-
-ALTER TABLE payload_statuses
-    ADD CONSTRAINT payload_statuses_status_id_fkey FOREIGN KEY (status_id) REFERENCES statuses(id);
+ALTER TABLE public.payload_statuses
+    ADD CONSTRAINT payload_statuses_status_id_fkey FOREIGN KEY (status_id) REFERENCES public.statuses(id);
 
 
 SELECT create_partition(NOW()::DATE, NOW()::DATE + interval '1 DAY');
