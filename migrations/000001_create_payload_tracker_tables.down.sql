7dfeb9e27d73c1f7567e18b9e09dc582403a9ae2
Derek Horton <dehort@redhat.com>
2025-08-25 14:49:03 -0500

    Downgrade seems to work

--- migrations/000001_create_payload_tracker_tables.down.sql
+++ migrations/000001_create_payload_tracker_tables.down.sql
@@ -3,16 +3,7 @@ DROP FUNCTION drop_partition;
 DROP FUNCTION get_date_string;
 
 DROP TABLE payload_statuses;
-DROP SEQUENCE payload_statuses_id_seq;
-
 DROP TABLE payloads;
-DROP SEQUENCE payloads_id_seq;
-
 DROP TABLE services;
-DROP SEQUENCE services_id_seq;
-
 DROP TABLE sources;
-DROP SEQUENCE sources_id_seq;
-
 DROP TABLE statuses;
-DROP SEQUENCE statuses_id_seq;
